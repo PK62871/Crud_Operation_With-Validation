27b9acbf196d419df5f9a64181922dda6b209f98
Prabhakar <prabhakar8102567@gmail.com>
2024-07-13 21:33:27 +0530

    Exception is added

--- src/main/java/com/validation/controller/UserController.java
+++ src/main/java/com/validation/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.validation.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +30,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	
+	//Save The Users Record.
+	
 	@PostMapping("/save")
 	public ResponseEntity<?> createUser(@Valid @RequestBody UserDto dto, BindingResult result) {
 
@@ -47,5 +56,47 @@ public class UserController {
 		
 		return new ResponseEntity<>("User is valid & Saved Successfully",HttpStatus.OK);
 
+	}
+	
+	// Get All Users Record.
+	
+	@GetMapping("/getuser")
+	public ResponseEntity<?> getAllUsers(){
+		List<User> allUser = userService.findAllUser();
+	
+		return new ResponseEntity<>(allUser,HttpStatus.OK);
+		
+	}
+	
+	// Find User By Id.
+	@GetMapping("/user/{id}")
+	public User getUserById(@PathVariable Integer id) {
+		User userById = userService.findUserById(id);
+		return userById;
+		
+	}
+	
+	//Delete User By Id.
+	@DeleteMapping("/delete/{id}")
+	public String deleteUser(@PathVariable Integer id) {
+		userService.deleteUser(id);
+		return "User Deleted Successfully";
+	}
+	
+	
+	//Update User.
+	@PutMapping("/update/{id}")
+	public ResponseEntity<?> updateUser(@PathVariable Integer id,@Valid @RequestBody UserDto userDto,BindingResult result) {
+		if (result.hasErrors()) {
+			Map<String, String> errors = new HashMap<String, String>();
+			for (FieldError error : result.getFieldErrors()) {
+				errors.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+		}
+
+		User updateUser = userService.updateUser(id, userDto);
+		return new ResponseEntity<>(updateUser,HttpStatus.OK);
+		
 	}
 }
--- src/main/java/com/validation/service/UserService.java
+++ src/main/java/com/validation/service/UserService.java
@@ -1,9 +1,15 @@
 package com.validation.service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.validation.dto.UserDto;
 import com.validation.entity.User;
+import com.validation.exception.UserNotFoundException;
 import com.validation.repository.UserRepository;
 
 @Service
@@ -19,4 +25,42 @@ public class UserService {
 		return saved;
 		
 	}
-}
+	
+	// Get Data.
+	public List<User> findAllUser(){
+		List<User> allUsers = userRepository.findAll();
+		return allUsers;
+		
+	}
+	
+	// Get User By Id.
+	public User findUserById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userOptional.orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+    }
+    
+
+		// Delete User Record By Id.
+	public void deleteUser(Integer id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+		userRepository.delete(user);
+		
+	}
+	
+	// Update User Record.
+	
+	public User updateUser(Integer id, UserDto userDto) {
+		User existingUser = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+		existingUser.setAddress(userDto.getAddress());
+		existingUser.setName(userDto.getName());
+		existingUser.setAge(userDto.getAge());
+		existingUser.setEmail(userDto.getEmail());
+		
+		User updatedUser = userRepository.save(existingUser);
+		
+		return updatedUser;
+		
+	}
+		
+	}
+
